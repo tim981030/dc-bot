cb946834cb2d9e1c517914c8af092722fad60013
tim981030 <126941384+tim981030@users.noreply.github.com>
2025-12-05 19:03:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from threading import Thread
 
 # --- Discord Token ---
 load_dotenv()
-token = os.getenv("token")  # 確認 Render 環境變數也是 token（小寫）
+token = os.getenv("token")
 
 if token is None:
     raise ValueError("Discord token not found! Please set 'token' in environment variables.")
@@ -18,24 +18,34 @@ intents.message_content = True
 intents.guild_messages = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-n=1
+
+# 記數變數
+n = 1
+
 @bot.event
 async def on_message(message):
     global n
-    content = message.content
+
+    # 忽略機器人訊息
     if message.author.bot:
         return
+
+    # 限定頻道
     if message.channel.id != 1446132825127387288:
         return
+
+    content = message.content
+
+    # 判斷數字順序
     if str(n) == content:
         n += 1
         await message.add_reaction("✅")
+
     else:
         n = 1
         await message.add_reaction("❌")
         await message.channel.send("錯了 你將受到森林的嚴厲斥責")
 
-    await bot.process_commands(message)
 # --- Flask Web Service for Render ---
 app = Flask("")
 
@@ -55,7 +65,3 @@ flask_thread.start()
 # --- Run Discord Bot ---
 print("Starting Discord bot...")
 bot.run(token)
-
-
-
-
